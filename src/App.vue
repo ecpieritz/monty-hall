da2147821623704389cdb323b2a05dd1a2d8c73b
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-08-25 17:04:00 -0300

    Added project base

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <h1>Monty Hall Problem</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {}
+}
+</script>
+
+<style>
+
+</style>
