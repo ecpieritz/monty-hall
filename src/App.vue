cebdcddc9ae6764bc4e09f48aae0d8fa902c37ef
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-08-25 17:08:55 -0300

    Added initial style

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,33 @@
 </template>
 
 <script>
-export default {
-  name: 'App',
-  components: {}
-}
+  export default {
+    name: "App",
+    components: {},
+  };
 </script>
 
 <style>
+  * {
+    font-family: "Montserrat", sans-serif;
+    box-sizing: border-box;
+  }
 
+  body {
+    color: #fff;
+    background: linear-gradient(90deg, #222424 10%, #b8b5ae 90%);
+  }
+
+  #app {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  #app h1 {
+    border: 1px solid #000;
+    background-color: #0004;
+    padding: 20px;
+    margin-bottom: 60px;
+  }
 </style>
