fba3d90e8f9ac89966da1c481539724c072dd983
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-08-26 11:29:21 -0300

    Created Gift Style

--- src/components/Gift.vue
+++ src/components/Gift.vue
@@ -9,10 +9,44 @@
 
 <script>
 export default {
-
+  name: 'Gift',
+  
 }
 </script>
 
 <style>
+.gift{
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  z-index: 10;
+}
 
+.gift-top{
+  width: 100px;
+  height: 25px;
+  background-color: #d15a96;
+}
+
+.gift-body{
+  width: 90px;
+  height: 60px;
+  background-color: #b6487f;
+}
+
+.gift-tie-1{
+  position: absolute;
+  width: 15px;
+  height: 85px;
+  background-color: #4885b6;
+}
+
+.gift-tie-2{
+  position: absolute;
+  top: 40px;
+  width: 90px;
+  height: 15px;
+  background-color: #4885b6;
+}
 </style>
