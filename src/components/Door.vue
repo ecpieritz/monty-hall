a8ecb94dc6e68861a22df6bd37e772e73a5c1347
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-08-26 13:18:49 -0300

    Added Door Style

--- src/components/Door.vue
+++ src/components/Door.vue
@@ -30,5 +30,51 @@ export default {
 </script>
 
 <style>
+:root{
+  --door-border: 5px solid #412317;
+}
 
+.door-area{
+  position: relative;
+  width: 200px;
+  height: 310px;
+  border-bottom: 10px solid #AAA;
+  margin-bottom: 20px;
+  font-size: 3rem;
+  display: flex;
+  justify-content: center;
+}
+
+.door-frame{
+  position: absolute;
+  height: 300px;
+  width: 180px;
+  border-left: var(--door-border);
+  border-right: var(--door-border);
+  border-top: var(--door-border);
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+}
+
+.door{
+  position: absolute;
+  top: 5px;
+  height: 295px;
+  width: 170px;
+  background-color: #583d33;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+}
+
+.door .knob{
+  height: 20px;
+  width: 20px;
+  border-radius: 10px;
+  background-color: #412317;
+  align-self: flex-start;
+  margin-top: 60px;
+}
 </style>
